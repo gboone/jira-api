8753512f1382ff9f23a5217691879e51f1053498
gboone <gregory.boone@cfpb.gov>
2014-05-06 10:23:41 -0400

    Adding the initial file

---
+++ jira-api.php
@@ -0,0 +1,13 @@
+/*
+Plugin Name: JIRA API Integration
+Plugin URI: http://github.com/gboone/jira-api
+Description: Adds hooks to comments and posts to integrate with JIRA
+
+With this plugin active, use the [jira] shortcode to add comments to an issue in
+a connected JIRA installation.
+
+Version: 1.0
+Author: Greg Boone
+Author URI: http://greg.harmsboone.org
+License: Public Domain Work of the Federal Government
+*/
